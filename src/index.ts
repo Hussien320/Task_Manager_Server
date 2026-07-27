03aec365b2ff394fc7603ed05db88e261d9b4c3f
Hussien320 <husssienzoughaib@gmail.com>
2026-07-27 22:09:03 +0300

    git commit -m "Add ApiException class and error handling middleware"

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import helmet from "helmet";
 import config from "./config";
+import express, { Request, Response, NextFunction } from "express";
 import logger from "./util/logger";
-import express from "express";
+
 import bodyParser from "body-parser";
 import cors from "cors";
 import requestLogger from "./middleware/requestlogger";
 import router from "./routes";
+import { ApiException } from "./util/exceptions/ApiException";
 const app=express();
 app.use(helmet());
 app.use(bodyParser.json());
@@ -13,7 +15,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(requestLogger);
 app.use("/", router);
-
+app.use((req, res) => {
+    res.status(404).json({ error: "Not Found" });
+});
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof ApiException) {
+        const apiExeption = err as ApiException;
+        logger.error("API Exception of status %d: %s", apiExeption.status, err.message);
+        res.status(apiExeption.status).json({ error: err.message });
+    } else {
+        logger.error("Unhandled Error: %s", err.message);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
 app.listen(config.port, () => {
   logger.info(`Server is running on http://${config.host}:${config.port}`);
 });
