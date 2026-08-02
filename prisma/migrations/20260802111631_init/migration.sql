09ad217fb8f8fd1e5b011dae8678d0e855e3c941
Hussien320 <husssienzoughaib@gmail.com>
2026-08-02 14:32:51 +0300

    added my models and applied migrations

---
+++ prisma/migrations/20260802111631_init/migration.sql
@@ -0,0 +1,95 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('EMPLOYEE', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "ProductType" AS ENUM ('PLASTIC', 'VEGETABLES', 'CLEANING');
+
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('ADDITION', 'REMOVAL');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "pass_hash" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'EMPLOYEE',
+    "is_verified" BOOLEAN NOT NULL DEFAULT false,
+    "last_login" TIMESTAMP(3),
+    "reset_password_token" TEXT,
+    "reset_password_expires_at" TIMESTAMP(3),
+    "refresh_token" TEXT,
+    "refresh_token_expires_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Supplier" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "product_type" "ProductType" NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Supplier_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "supplier_id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "Type" "ProductType" NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "expiry_date" TIMESTAMP(3),
+    "low_stock_threshold" INTEGER,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "InventoryLog" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "product_id" TEXT NOT NULL,
+    "transaction_type" "TransactionType" NOT NULL,
+    "quantity_changed" INTEGER NOT NULL,
+    "unit_price_at_time" DECIMAL(65,30) NOT NULL,
+    "logged_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "InventoryLog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AppSetting" (
+    "id" TEXT NOT NULL,
+    "updated_by" TEXT,
+    "setting_key" TEXT NOT NULL,
+    "setting_value" TEXT NOT NULL,
+    "updated_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AppSetting_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AppSetting_setting_key_key" ON "AppSetting"("setting_key");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_supplier_id_fkey" FOREIGN KEY ("supplier_id") REFERENCES "Supplier"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "InventoryLog" ADD CONSTRAINT "InventoryLog_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "InventoryLog" ADD CONSTRAINT "InventoryLog_product_id_fkey" FOREIGN KEY ("product_id") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AppSetting" ADD CONSTRAINT "AppSetting_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
